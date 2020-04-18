1a752085f545c952e923e3fe868a91e03fd2c4e9
zqlu <zqlu.cn@gmail.com>
2020-04-18 19:37:45 +0800

    feat: add jdk function and complete

--- completions/jdk.fish
+++ completions/jdk.fish
@@ -5,3 +5,11 @@
 #
 # If your package doesn't provide any command line utility,
 # feel free to remove completions directory from the project.
+
+complete -c jdk -f -d "Switch between different JDK versions."
+complete -c jdk -f -a 1.8
+complete -c jdk -f -a 9
+complete -c jdk -f -a 10
+complete -c jdk -f -a 11
+complete -c jdk -f -a 12
+complete -c jdk -f -a 13
--- functions/jdk.fish
+++ functions/jdk.fish
@@ -1,3 +1,15 @@
-function jdk -d "My package"
+function jdk -d "switch between different JDK versions"
   # Package entry-point
+  set jdkv $argv[1]
+  if test -z "$jdkv"
+    # default version
+    echo "Switching to default JDK version..."
+    set -gx JAVA_HOME (/usr/libexec/java_home)
+  else
+    # specific version
+    echo "Switching to JDK version $jdkv"
+    set -gx JAVA_HOME (/usr/libexec/java_home -v $jdkv)
+  end
+  echo "Call java -version ..."
+  java -version
 end
